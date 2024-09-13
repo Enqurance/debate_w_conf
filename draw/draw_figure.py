0eceff295c484748a337486d13f8c19abd3e3406
Enqurance <Enqurance@outlook.com>
2024-09-13 09:38:42 +0800

    Clean working tree

--- draw/draw_figure.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-penguins = sns.load_dataset("penguins")
-sns.histplot(data=penguins, x="flipper_length_mm", hue="species", multiple="stack")
-
-print(type(penguins))
-print(penguins)
-plt.show()
